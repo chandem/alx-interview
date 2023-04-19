3136bbfa57a7c01f3f339c6bee2ee3c48b6a7aef
Chane Eshetu <49794721+chandem@users.noreply.github.com>
2023-04-19 10:12:12 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,19 +3,19 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    pascal_triangle = [0] * n
+    triangle = []
 
     for i in range(n):
-        row = [0] * (i+1)
+        row = [0] * (i + 1)
         row[0] = 1
-        row[len(row) - 1] = 1
+        row[-1] = 1
 
         for j in range(1, i):
             if j > 0 and j < len(row):
-                a = pascal_triangle[i - 1][j]
-                b = pascal_triangle[i - 1][j - 1]
+                a = triangle[i - 1][j]
+                b = triangle[i - 1][j - 1]
                 row[j] = a + b
 
-        pascal_triangle[i] = row
+        triangle.append(row)
 
-    return pascal_triangle
+    return triangle
