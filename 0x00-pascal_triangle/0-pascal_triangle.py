848bc670d6c11eabdc6ab744bc76e3a60c794266
Chane Eshetu <49794721+chandem@users.noreply.github.com>
2023-04-19 06:27:30 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,13 @@
 def pascal_triangle(n):
     if n <= 0:
         return []
-    # create the first row of the triangle
     row = [1]
-    # create a list of lists to store the triangle
     triangle = [row]
-    # calculate the rest of the rows
     for i in range(1, n):
-        # create a new row
         row = [1]
-        # calculate the values in the row
         for j in range(1, i):
-            # each value is the sum of the two values above it
             value = triangle[i-1][j-1] + triangle[i-1][j]
             row.append(value)
         row.append(1)
-        # add the row to the triangle
         triangle.append(row)
     return triangle
