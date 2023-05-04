e039d7d23bfdbb9864e6fee6598261bb8228d6f0
Chane Eshetu <49794721+chandem@users.noreply.github.com>
2023-05-04 11:32:36 +0300

    Create 0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,39 @@
+
+
+def minOperations(n):
+
+    if n == 1:
+
+        return 0
+
+    x = 1
+
+    ans = float('inf')
+
+    while x <= n:
+
+        if n % x == 0:
+
+            ans = min(ans, n//x - 1)
+
+        x *= 2
+
+    if ans != float('inf'):
+
+        return ans
+
+    for x in range(n//2, 0, -1):
+
+        if n % x == 0:
+
+            f = n // x
+
+            ans = min(ans, f*x//f - x + minOperations(f))
+
+    if ans != float('inf'):
+
+        return ans
+
+    return 0
+
+Copy All => Paste => HH => Paste =>HHH => Copy All => Paste => HHHHHH => Paste => HHHHHHHHH
